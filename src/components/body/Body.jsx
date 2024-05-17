3639ba6e08cb9ff317fadc8e6da5b2c1d2526210
sohagbhuiyan <smsohag171@gmail.com>
2024-05-17 22:54:31 +0600

    new task added

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -3,11 +3,14 @@ import "./body.css";
 import pic from "../../assets/Images.png";
 const Body = () => {
   return (
-    <div className="body">
-      <div className="image">
+    <div className="main">
         <img src={pic} className="img" />
+<div className="right">
+<p className="text-big">Take a taste, come join us. Life is so endlessly delicious </p>
+<p className="text-small">In the artist's own experience, of course, art is fundamentally indefinable, unsayable; there is something sacred about its demands upon the soul, something inherently mysterious in the forms it takes</p>
+<button className="btn-dwn">Download our App</button>
+</div>
       </div>
-    </div>
   );
 };
 
